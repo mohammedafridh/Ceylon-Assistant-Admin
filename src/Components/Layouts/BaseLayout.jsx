b1710047e204e89f6746fb0edd6898c614081560
Afridh <afridh7788@gmail.com>
2023-07-30 23:09:00 +0530

    added new changes

--- src/Components/Layouts/BaseLayout.jsx
+++ src/Components/Layouts/BaseLayout.jsx
@@ -4,17 +4,17 @@ import TopBar from "./TopBar";
 export default function BaseLayout(props) {
   return (
     <div className="adminPanel">
-        <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
-        <TopBar />
-        <div className="containers">
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
+      <TopBar />
+      <div className="containers">
         <div className="sideBarContainer">
-            <SideBar />
-          </div>
-          <div className="allPageContainer">
-            {props.children}
-          </div>
+          <SideBar />
         </div>
-     
+        <div className="allPageContainer">
+          {props.children}
+        </div>
+      </div>
+
     </div>
   );
 }
